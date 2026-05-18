2283cb826a5ff74f7835f2c13e53c3fbfbc33112
Jay Hogg <jay.hogg@gcsu.edu>
2026-05-18 15:32:58 -0400

    security added apk

--- models/Report.js
+++ models/Report.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const reportSchema = new mongoose.Schema({
   type: {
     type: String,
-    enum: ['user', 'shoutout', 'lost', 'market', 'comment'],   // ← Added new types
+    enum: ['user', 'shoutout', 'lost', 'market', 'comment', 'event', 'deal', 'news'],
     required: true
   },
 
@@ -15,15 +15,18 @@ const reportSchema = new mongoose.Schema({
   },
 
   // References
-  reportedUser:      { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  reportedShoutout:  { type: mongoose.Schema.Types.ObjectId, ref: 'Shoutout' },
-  reportedLostItem:  { type: mongoose.Schema.Types.ObjectId, ref: 'LostItem' },
-  reportedMarketItem:{ type: mongoose.Schema.Types.ObjectId, ref: 'MarketplaceItem' },
-  reportedComment:   { type: mongoose.Schema.Types.ObjectId },
+  reportedUser:      { type: mongoose.Schema.Types.ObjectId, ref: 'User',            default: null },
+  reportedShoutout:  { type: mongoose.Schema.Types.ObjectId, ref: 'Shoutout',        default: null },
+  reportedLostItem:  { type: mongoose.Schema.Types.ObjectId, ref: 'LostItem',        default: null },
+  reportedMarketItem:{ type: mongoose.Schema.Types.ObjectId, ref: 'MarketplaceItem', default: null },
+  reportedComment:   { type: mongoose.Schema.Types.ObjectId,                         default: null },
+  reportedEvent:     { type: mongoose.Schema.Types.ObjectId, ref: 'Event',           default: null },
+  reportedDeal:      { type: mongoose.Schema.Types.ObjectId, ref: 'Deal',            default: null },
+  reportedNews:      { type: mongoose.Schema.Types.ObjectId, ref: 'News',            default: null },
 
   snapshotText: { type: String, default: '' },
   reason:       { type: String, required: true },
-  
+
   status: {
     type: String,
     enum: ['pending', 'reviewed', 'dismissed'],
@@ -35,9 +38,12 @@ const reportSchema = new mongoose.Schema({
 });
 
 // Indexes for performance + duplicate prevention
-reportSchema.index({ reporter: 1, reportedShoutout: 1 }, { sparse: true });
-reportSchema.index({ reporter: 1, reportedLostItem: 1 }, { sparse: true });
+reportSchema.index({ reporter: 1, reportedShoutout:   1 }, { sparse: true });
+reportSchema.index({ reporter: 1, reportedLostItem:   1 }, { sparse: true });
 reportSchema.index({ reporter: 1, reportedMarketItem: 1 }, { sparse: true });
-reportSchema.index({ reporter: 1, reportedUser: 1 }, { sparse: true });
+reportSchema.index({ reporter: 1, reportedUser:       1 }, { sparse: true });
+reportSchema.index({ reporter: 1, reportedEvent:      1 }, { sparse: true });
+reportSchema.index({ reporter: 1, reportedDeal:       1 }, { sparse: true });
+reportSchema.index({ reporter: 1, reportedNews:       1 }, { sparse: true });
 
 module.exports = mongoose.model('Report', reportSchema);
